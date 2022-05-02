1f4551fb100429a62113749ebfbfbe951b9901ce
Asish Binu Mathew <asish.bm@devbud.ai>
2022-05-03 02:08:08 +0530

    perm

--- streamingapp/views.py
+++ streamingapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from streamingapp.models import StreamHelper
 
 
@@ -9,9 +9,17 @@ from streamingapp.models import StreamHelper
 @authentication_classes([BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def updateCount(request):
+    # print(request.user.get_all_permissions())
+
+    if request.user.has_perm('streamingapp.view_streamhelper'):
+        print("has permission")
+    else:
+        print("has no permission")
+
     content = {
         'user': str(request.user),  # `django.contrib.auth.User` instance.
         'auth': str(request.auth),  # None
+        "perm": str(request.user.get_all_permissions())
     }
     stream, _ = StreamHelper.objects.get_or_create(user=request.user)
     print(_)
